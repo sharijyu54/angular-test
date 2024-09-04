37ea4ca8d13bc709a5384fa5567ac8a5854b30ac
yuji.shirai <arco.e.frecce@gmail.com>
2024-09-04 23:07:33 +0900

    remove unused module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskListItemComponent } from './task-list-item/task-list-item.component';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
@@ -12,7 +11,6 @@ import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
   imports: [
     RouterOutlet, 
     CommonModule, 
-    FormsModule,
     TaskListComponent,
     TaskListItemComponent,
     NzPageHeaderModule,
